0a15eb507b5578e6737e07a31175c2ee1426be8c
Stanislav Vorobjov <stsvorobev@gmail.com>
2017-12-12 13:23:00 +0500

    prepare to deploy 2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,5 +8,19 @@ set :deploy_to, '/usr/local/cityprice'
 
 set :repo_url, 'git@bitbucket.org:m_sanama/sauna_backend.git'
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/socket}
+set :linked_files, %w[config/database.yml config/secrets.yml]
+set :linked_dirs, %w[bin log tmp/pids tmp/cache tmp/socket]
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile do
+      on release_roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env), rails_groups: fetch(:rails_assets_groups) do
+            logger.info 'Skipping asset pre-compilation because it API application'
+          end
+        end
+      end
+    end
+  end
+end
