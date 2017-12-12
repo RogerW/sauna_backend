fc459c66466f256d7ec9a19833f4a4c0336517df
Stanislav Vorobjov <stsvorobev@gmail.com>
2017-12-12 11:48:08 +0500

    prepare to deploy 1

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,3 +5,8 @@ role :web, %w[roger@cityprice.club]
 role :db,  %w[roger@cityprice.club]
 
 set :deploy_to, '/usr/local/cityprice'
+
+set :repo_url, 'git@bitbucket.org:m_sanama/sauna_backend.git'
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/socket}
