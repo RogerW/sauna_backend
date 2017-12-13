49e7e1005112ec73796deffcb7eb2a054d3f5673
Stanislav Vorobjov <stsvorobev@gmail.com>
2017-12-13 16:40:36 +0500

    change puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 environment ENV['RAILS_ENV'] || 'production'
-pidfile '/usr/local/cityprice/current/shared/tmp/pids/puma.pid'
-stdout_redirect '/usr/local/cityprice/current/shared/tmp/log/stdout', '/usr/local/cityprice/current/shared/tmp/log/stderr'
+pidfile '/usr/local/cityprice/shared/tmp/pids/puma.pid'
+stdout_redirect '/usr/local/cityprice/shared/tmp/log/stdout', '/usr/local/cityprice/shared/tmp/log/stderr'
 threads 2, 16
 workers 2
-bind 'unix:///usr/local/cityprice/current/shared/tmp/socket/puma.sock'
+bind 'unix:///usr/local/cityprice/shared/tmp/socket/puma.sock'
 # daemonize true
